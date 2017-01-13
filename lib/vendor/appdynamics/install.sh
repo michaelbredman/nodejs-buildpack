ed477495adb0eeb30d77e314ee8b3c80c84b949b
Michael Redman <mredman@gmail.com>
2017-01-13 15:42:56 -0800

    yet anohter

--- lib/vendor/appdynamics/install.sh
+++ lib/vendor/appdynamics/install.sh
@@ -27,7 +27,7 @@ then
   APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(echo "${VCAP_SERVICES-}" | $JQ --raw-output '.['\""$key"\"'][0] | .credentials | .["account-access-key"] ')
   APPDYNAMICS_AGENT_APPLICATION_NAME=$(echo "${VCAP_APPLICATION-}" | $JQ --raw-output .application_name)
   APPDYNAMICS_AGENT_TIER_NAME=$(echo "${VCAP_APPLICATION-}" | $JQ --raw-output .application_name)
-  APPDYNAMICS_AGENT_NODE_NAME=$(echo "${CF_INSTANCE_GUID-}")
+  APPDYNAMICS_AGENT_NODE_NAME=$(echo "$CF_INSTANCE_GUID")
 
   if [ ! -z "${APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY-}" ];
   then
